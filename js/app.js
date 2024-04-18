192503b5d31c38ee30e4386ad4e0d33c3ebaaaac
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-04-18 15:33:27 -0300

    Commit inicial

---
+++ js/app.js
@@ -0,0 +1,31 @@
+let totalGeral = 0;
+
+function adicionar() {
+    let quantidade = document.getElementById('quantidade').value;
+    let campoProduto = document.getElementById('produto').value;
+    let produto = campoProduto.split('-')[0];
+    let valorUni = campoProduto.split('R$')[1];
+    let valor = quantidade * valorUni;
+    let listaDoCarrinho = document.getElementById('lista-produtos');
+    let valorTotal = document.getElementById('valor-total');
+    if(quantidade > 0){
+        listaDoCarrinho.innerHTML += `
+        <section class="carrinho__produtos__produto">
+            <span class="texto-azul">${quantidade}x</span> ${produto} <span class="texto-azul">R$${valorUni}</span>
+        </section>
+        `
+    }else{
+        alert('Digite a quantidade de produtos.');
+    }
+
+    document.getElementById('quantidade').value = '';
+    totalGeral = totalGeral + valor;
+    valorTotal.innerText = `R$${totalGeral}`;
+}
+
+
+function limpar() {
+    totalGeral = 0;
+    document.getElementById('valor-total').innerText = 'R$0';
+    document.getElementById('lista-produtos').innerHTML = '';
+}
